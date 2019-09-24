0645e708348dd37e09459478a063907b3e4f7419
Gorgchap <gorgchap@gmail.com>
2019-09-24 17:38:14 +0300

    Project-edit

--- src/app/project-list/project-edit/project-edit.component.ts
+++ src/app/project-list/project-edit/project-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators} from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import * as moment from 'moment';
 import { Subscription } from 'rxjs';
 import { Project } from '../../project.service';
 
@@ -21,11 +22,23 @@ export class ProjectEditComponent implements OnInit {
   ngOnInit(): void {
     this.form = this.builder.group( {
       id: [this.data.id],
-      date_begin: [this.data.date_begin || new Date(), [Validators.required]],
-      date_end: [this.data.date_end || new Date(), [Validators.required]],
-      cost: [this.data.cost || 10000, [Validators.required, Validators.min(10000)]],
-      comment: [this.data.comment]
+      date_begin: [this.data.date_begin || new Date(), [Validators.required, this.dateValidator]],
+      date_end: [this.data.date_end || new Date(), [Validators.required, this.dateValidator]],
+      cost: [this.data.cost || 1000, [Validators.required, Validators.min(1000)]],
+      comment: [this.data.comment, [Validators.maxLength(512)]]
     });
+    this.subscription.add(
+      this.form.get('date_begin').valueChanges.subscribe(() => this.form.get('date_end').updateValueAndValidity({ emitEvent: false }))
+    ).add(
+      this.form.get('date_end').valueChanges.subscribe(() => this.form.get('date_begin').updateValueAndValidity({ emitEvent: false }))
+    );
+  }
+
+  dateValidator(control: AbstractControl): {[key: string]: any} | null {
+    if (control.parent && moment(control.parent.get('date_begin').value).isAfter(control.parent.get('date_end').value)) {
+      return { dateError: true };
+    }
+    return null;
   }
 
   submit() {
